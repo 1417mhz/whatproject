e5a7f570066a5c831e6f647f02ce763cd8567b13
1417mhz <ysw99@kakao.com>
2023-07-22 13:03:46 +0900

    사용자 등급 도메인 수정

---
+++ src/main/java/myproject/whatproject/domain/user/UserRole.java
@@ -0,0 +1,14 @@
+package myproject.whatproject.domain.user;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+public class UserRole {
+
+    private String role_name;
+    private String description;
+
+}
