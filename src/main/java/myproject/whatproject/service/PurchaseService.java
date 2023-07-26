df1ebfea71a7a0cc5199d5465de6497574f4eab5
1417mhz <ysw99@kakao.com>
2023-07-26 13:27:20 +0900

    커스텀 예외 도입 일부 코드 표기법 수정 및 최적화

--- src/main/java/myproject/whatproject/service/PurchaseService.java
+++ src/main/java/myproject/whatproject/service/PurchaseService.java
@@ -5,6 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import myproject.whatproject.domain.product.Product;
 import myproject.whatproject.domain.purchase.Purchase;
 import myproject.whatproject.domain.user.User;
+import myproject.whatproject.exception.ProductDataNotFoundException;
+import myproject.whatproject.exception.UserDataNotFoundException;
+import myproject.whatproject.exception.ProductStockNotEnoughException;
+import myproject.whatproject.exception.UserBalanceNotEnoughException;
 import myproject.whatproject.mapper.MyMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,32 +31,36 @@ public class PurchaseService {
         int orderQuantity = purchase.getQuantity(); // 주문 수량
         int totalCheckout = purchase.getPrice() * orderQuantity; // 총 금액
 
-        // 상품 데이터, 구매자(회원) 데이터 유효성 검증
-        Product product = myMapper.findProductByNum(purchase.getProduct_no());
+        Product product = myMapper.findProductByNum(purchase.getProductNum());
         User buyer = myMapper.findUserByNum(purchase.getBuyer());
-        if (product == null || buyer == null) {
-            log.error("!! 입력된 정보에 해당하는 데이터가 없습니다 !!");
-            throw new RuntimeException();
+
+        // 상품 데이터, 구매자(회원) 데이터 유효성 검증
+        if (product == null) {
+            log.error("!! 입력된 정보에 해당하는 상품이 없습니다 !!");
+            throw new ProductDataNotFoundException("!! 입력된 정보에 해당하는 상품이 없습니다 !!");
+        } else if (buyer == null) {
+            log.error("!! 입력된 정보에 해당하는 회원이 없습니다 !!");
+            throw new UserDataNotFoundException("!! 입력된 정보에 해당하는 회원이 없습니다 !!");
         }
+        int newQuantity = product.getQuantity() - orderQuantity;
+        int newUserBalance = buyer.getBalance() - totalCheckout;
 
         // 재고 유효성 검증
-        int newQuantity = product.getQuantity() - orderQuantity;
         if (newQuantity < 0) {
             log.error("!! 주문 등록 실패 !! 재고와 주문 수량을 확인해주세요");
-            throw new RuntimeException("!! 주문 수량보다 재고가 적음 !!");
+            throw new ProductStockNotEnoughException("!! 주문 수량보다 재고가 적음 !!");
         }
-        myMapper.updateProductQuantity(purchase.getProduct_no(), Integer.toString(newQuantity));
 
         // 적립금 유효성 검증
-        int newUserBalance = buyer.getBalance() - totalCheckout;
         if (newUserBalance < 0) {
             log.error("!! 주문 등록 실패 !! 적립금이 부족합니다");
-            throw new RuntimeException("!! 주문 금액보다 적립금이 적음 !!");
+            throw new UserBalanceNotEnoughException("!! 주문 금액보다 적립금이 적음 !!");
         }
-        myMapper.updateUserBalance(buyer.getUser_no(), Integer.toString(newUserBalance));
 
         // 주문 성공
         myMapper.createNewOrder(purchase);
+        myMapper.updateProductQuantity(purchase.getProductNum(), Integer.toString(newQuantity));
+        myMapper.updateUserBalance(buyer.getUserNum(), Integer.toString(newUserBalance));
 
         log.info("** 주문 생성 완료 **");
     }
