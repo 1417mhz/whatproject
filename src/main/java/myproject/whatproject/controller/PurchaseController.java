a338fdb60bcd75f27b0399110b100879b320a650
1417mhz <ysw99@kakao.com>
2023-08-04 01:01:55 +0900

    상품 주문 서비스 개선 및 구현 1. 주문 기능 개선 및 최적화 2. 주문 취소 기능 구현 3. 주문 페이지 및 주문 정보 조회 구현

--- src/main/java/myproject/whatproject/controller/PurchaseController.java
+++ src/main/java/myproject/whatproject/controller/PurchaseController.java
@@ -1,12 +1,15 @@
 package myproject.whatproject.controller;
 
 import lombok.RequiredArgsConstructor;
+import myproject.whatproject.domain.purchase.ProductToPurchaseVO;
 import myproject.whatproject.domain.purchase.Purchase;
 import myproject.whatproject.service.PurchaseService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.List;
 
 @Controller
 @RequestMapping("/order")
@@ -16,15 +19,51 @@ public class PurchaseController {
     private final PurchaseService purchaseService;
 
     @GetMapping("/new")
-    public String createNewOrderForm() {
+    public String createNewOrderForm(ProductToPurchaseVO orderInfo, Principal principal, Model model) {
+        model.addAttribute("user", purchaseService.getUserInstance(principal.getName())); // 로그인 되어있는 유저
+        model.addAttribute("product", purchaseService.getProductInstance(orderInfo.getProductNo()));
+        model.addAttribute("orderInfo", orderInfo);
+        model.addAttribute("totalCheckout", orderInfo.getPrice() * orderInfo.getOrderQuantity()); // 총 결제 금액
         return "/purchase/createNewOrderForm";
     }
-    // 상품 정보에서 주문을 클릭했을 때 상품의 정보를 어떻게 넘겨 결제를 발생시킬지 생각해보셈
 
     @PostMapping("/new")
     public String createNewOrder(Purchase purchase) {
         purchaseService.createNewOrder(purchase);
         return "redirect:/";
+    }
+
+    @PostMapping("/cancel")
+    public String cancelOrder(int purchaseNo) {
+        purchaseService.cancelOrder(purchaseNo);
+        return "redirect:/order/list";
+    }
+
+    @GetMapping("/list")
+    public String redirectToOrderList(Principal principal) {
+        int userNo = purchaseService.getUserInstance(principal.getName()).getUserNo();
+        return "redirect:/order/list/user/" + userNo;
+    }
+
+    @GetMapping("/list/all") // 어드민페이지
+    public String getOrderList(Model model) {
+        List<Purchase> orders = purchaseService.findAllOrder();
+        model.addAttribute("orders", orders);
+        return "/purchase/orders";
+    }
+
+    @GetMapping("/list/{purchaseNo}")
+    public String getOrderInfo(@PathVariable int purchaseNo, Model model) {
+        Purchase order = purchaseService.findOrderByNo(purchaseNo);
+        model.addAttribute("order", order);
+        return "/purchase/order";
+    }
+
+    @GetMapping("/list/user/{userNo}")
+    public String getUserOrderList(@PathVariable int userNo, Model model) {
+        List<Purchase> orders = purchaseService.findUserOrder(userNo);
+        model.addAttribute("orders", orders);
+        return "/purchase/orders";
     }
 
 }
--- src/main/java/myproject/whatproject/service/PurchaseService.java
+++ src/main/java/myproject/whatproject/service/PurchaseService.java
@@ -5,10 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import myproject.whatproject.domain.product.Product;
 import myproject.whatproject.domain.purchase.Purchase;
 import myproject.whatproject.domain.user.User;
-import myproject.whatproject.exception.ProductDataNotFoundException;
-import myproject.whatproject.exception.UserDataNotFoundException;
-import myproject.whatproject.exception.ProductStockNotEnoughException;
-import myproject.whatproject.exception.UserBalanceNotEnoughException;
+import myproject.whatproject.exception.*;
 import myproject.whatproject.mapper.MyMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,47 +18,97 @@ import java.util.List;
 public class PurchaseService {
 
     private final MyMapper myMapper;
+    private final UserService userService;
+    private final ProductService productService;
 
     public List<Purchase> findAllOrder() {
         return myMapper.findAllOrder();
     }
 
+    public Purchase findOrderByNo(int purchaseNo) {
+        return myMapper.findOrderByNo(purchaseNo);
+    }
+
+    public List<Purchase> findUserOrder(int userNo) {
+        return myMapper.findUserOrder(userNo);
+    }
+
+    public User getUserInstance(String userId) {
+        return userService.findUserById(userId);
+    }
+
+    public Product getProductInstance(int productNo) {
+        return productService.findProductByNo(productNo);
+    }
+
     @Transactional
     public void createNewOrder(Purchase purchase) throws RuntimeException {
-        int orderQuantity = purchase.getQuantity(); // 주문 수량
-        int totalCheckout = purchase.getPrice() * orderQuantity; // 총 금액
-
-        Product product = myMapper.findProductByNum(purchase.getProductNum());
-        User buyer = myMapper.findUserByNum(purchase.getBuyer());
+        Product product = myMapper.findProductByNo(purchase.getProductNo());
+        User buyer = myMapper.findUserById(purchase.getBuyerId());
 
         // 상품 데이터, 구매자(회원) 데이터 유효성 검증
         if (product == null) {
             log.error("!! 입력된 정보에 해당하는 상품이 없습니다 !!");
-            throw new ProductDataNotFoundException("!! 입력된 정보에 해당하는 상품이 없습니다 !!");
-        } else if (buyer == null) {
-            log.error("!! 입력된 정보에 해당하는 회원이 없습니다 !!");
-            throw new UserDataNotFoundException("!! 입력된 정보에 해당하는 회원이 없습니다 !!");
+            throw new ProductDataNotFoundException();
         }
-        int newQuantity = product.getQuantity() - orderQuantity;
+        if (buyer == null) {
+            log.error("!! 입력된 정보에 해당하는 회원이 없습니다 !!");
+            throw new UserDataNotFoundException();
+        }
+        purchase.setBuyer(buyer.getUserNo()); // form에서 넘어온 사용자 id로 사용자 번호를 알아내서 set함
+        int orderQuantity = purchase.getOrderQuantity(); // 주문 수량
+        int totalCheckout = purchase.getOrderQuantity() * product.getPrice(); // 총 결제 금액
+
+        int newProductStock = product.getProductStock() - orderQuantity;
         int newUserBalance = buyer.getBalance() - totalCheckout;
 
         // 재고 유효성 검증
-        if (newQuantity < 0) {
+        if (newProductStock < 0) {
             log.error("!! 주문 등록 실패 !! 재고와 주문 수량을 확인해주세요");
-            throw new ProductStockNotEnoughException("!! 주문 수량보다 재고가 적음 !!");
+            throw new ProductStockNotEnoughException();
         }
 
         // 적립금 유효성 검증
         if (newUserBalance < 0) {
             log.error("!! 주문 등록 실패 !! 적립금이 부족합니다");
-            throw new UserBalanceNotEnoughException("!! 주문 금액보다 적립금이 적음 !!");
+            throw new UserBalanceNotEnoughException();
         }
 
         // 주문 성공
         myMapper.createNewOrder(purchase);
-        myMapper.updateProductQuantity(purchase.getProductNum(), Integer.toString(newQuantity));
-        myMapper.updateUserBalance(buyer.getUserNum(), Integer.toString(newUserBalance));
+        myMapper.updateProductStock(product.getProductNo(), newProductStock);
+        myMapper.updateUserBalance(buyer.getUserNo(), newUserBalance);
 
         log.info("** 주문 생성 완료 **");
+    }
+
+    @Transactional
+    public void cancelOrder(int purchaseNo) {
+        Purchase order = myMapper.findOrderByNo(purchaseNo);
+        if (order == null) {
+            log.error("!! 입력된 정보에 해당하는 구매 내역이 없습니다 !!");
+            throw new PurchaseDataNotFoundException();
+        }
+
+        User user = myMapper.findUserByNo(order.getBuyer());
+        Product product = myMapper.findProductByNo(order.getProductNo());
+
+        if (user == null) {
+            log.error("!! 입력된 정보에 해당하는 회원이 없습니다 !!");
+            throw new UserDataNotFoundException();
+        }
+        if (product == null) {
+            log.error("!! 입력된 정보에 해당하는 상품이 없습니다 !!");
+            throw new ProductDataNotFoundException();
+        }
+
+        int restoreBalance = user.getBalance() + order.getTotalCheckout();
+        int restoreQuantity = product.getProductStock() + order.getOrderQuantity();
+
+        myMapper.cancelOrder(purchaseNo);
+        myMapper.restoreUserBalance(restoreBalance, user.getUserNo());
+        myMapper.restoreProductQuantity(restoreQuantity, product.getProductNo());
+
+        log.info("** 주문 취소 완료 **");
     }
 }
