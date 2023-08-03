6b2afdf3c1e915939e8899945329debea1cf8f7f
1417mhz <ysw99@kakao.com>
2023-08-04 01:06:49 +0900

    쿼리 매핑 수정 1. 변수 타입, 이름 일부 변경에 따른 수정 2. 주문 관련 매핑 추가

--- src/main/java/myproject/whatproject/mapper/MyMapper.java
+++ src/main/java/myproject/whatproject/mapper/MyMapper.java
@@ -18,21 +18,23 @@ public interface MyMapper {
 
     List<User> findAllUser();
 
-    User findUserByNum(String userNum);
+    User findUserByNo(int userNo);
 
     User findUserById(String userId);
+
+    int findUserNoById(String userId);
 
     User findUserByEmail(String userEmail);
 
     String userVerify(String userId);
 
-    // 인자를 두 개 이상 전달 시 mapper 에서 인자 이름을 인식할 수 있도록 직접 이름을 지정
     void changeUserPw(@Param("userId") String userId, @Param("changePw") String changePw);
+
 
     /* 판매자 **/
     List<Seller> findAllSeller();
 
-    Seller findSellerByNum(String sellerNum);
+    Seller findSellerByNo(int sellerNo);
 
 
     /* 상품 **/
@@ -40,15 +42,26 @@ public interface MyMapper {
 
     List<Product> listAllProduct();
 
-    Product findProductByNum(String productNum);
+    Product findProductByNo(int productNo);
+
 
     /* 주문 **/
     List<Purchase> findAllOrder();
 
+    Purchase findOrderByNo(int purchaseNo);
+
+    List<Purchase> findUserOrder(int userNo);
+
     void createNewOrder(Purchase purchase);
 
-    void updateProductQuantity(@Param("productNum") String productNum, @Param("quantity") String orderQuantity);
+    void updateProductStock(@Param("productNo") int productNo, @Param("newProductStock") int newProductStock);
 
-    void updateUserBalance(@Param("userNum") String userNum, @Param("balance") String balance);
+    void updateUserBalance(@Param("userNo") int userNo, @Param("newUserBalance") int newUserBalance);
+
+    void cancelOrder(int purchaseNo);
+
+    void restoreProductQuantity(@Param("restoreQuantity") int restoreQuantity, @Param("productNo") int productNo);
+
+    void restoreUserBalance(@Param("restoreBalance") int restoreBalance, @Param("userNo") int userNo);
 
 }
